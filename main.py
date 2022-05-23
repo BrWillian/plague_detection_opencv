216c13ec67a7135435e39cc00201de044dd8b84a
brwillian <wiliam-m-@hotmail.com>
2022-05-22 23:16:28 -0300

    added parse functions on project

--- main.py
+++ main.py
@@ -4,17 +4,35 @@ from PlagueDetection import PlagueDetection
 
 
 def main():
-    img = cv2.imread('data/usa.jpg')
-    wd = PlagueDetection(img)
-    img_hsv = wd.preprocess_image(img)
-    msk = wd.create_mask(img_hsv)
-    msk = wd.transform_image(msk)
-    res_msk = cv2.bitwise_and(img, img, mask=msk)
-    cv2.imshow('Mask', res_msk)
-    cv2.waitKey()
-    cv2.imshow('Frame', img)
-    cv2.waitKey()
-    cv2.destroyAllWindows()
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument('--sensitivity', type=int, help='Sensibilidade do detector', default=30)
+    parser.add_argument('--circles', type=bool, help='Desenhar ou não circulo nas detecções', default=False)
+    parser.add_argument('--image', type=str, help='Caminho da imagem', required=True)
+
+    args = parser.parse_args()
+
+    # Load image
+    img_path = cv2.imread(args.image)
+
+    # Create object instance
+    plague_detection = PlagueDetection(img_path, circles=args.circles, sensitivity=args.sensitivity)
+
+    # Get image from preprocess output
+    img_hsv = plague_detection.preprocess_image(img_path)
+
+    # Get mask
+    msk = plague_detection.create_mask(img_hsv)
+
+    # Get mask before morphology transformation
+    msk = plague_detection.transform_image(msk)
+
+    # Get mask with drawings
+    result = plague_detection.draw_mask_color(msk, img_path)
+
+    # Show result
+    plague_detection.show_image(result)
 
 
 if __name__ == "__main__":
